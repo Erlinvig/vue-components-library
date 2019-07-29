ce331fe603b750ae25996043d031ce9b8e5f5077
erlinvig <erlinvig@gmail.com>
2019-07-29 20:46:57 +0300

    feat(ui): v-table  v-0-1

---
+++ src/components/library/table.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="table"
+         :style="styleTable"
+    >
+        <table>
+            <tr class="row" v-for="row in config.content">
+                <td class="el" v-for="el in row.row">
+                    <div v-if="el.value">
+                        <span>{{el.value}}</span>
+                    </div>
+                    <div v-if="el.component">
+                        <div :is="el.component"></div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['config'],
+        components: {
+
+        },
+        data() {
+            return {}
+        },
+
+        computed: {
+            styleTable() {
+                if (this.config.style) {
+                    let style = {};
+
+                    // In development
+                    //
+                    // function init(propertyName, propertyValue) {
+                    //     let property =  propertyValue ? propertyValue : null;
+                    //     if (property) { style[propertyName] = `${property}` }
+                    // }
+                    //
+                    // init('backgroundColor', this.config.style.backgroundColor);
+
+                    let borderSize = this.config.style.borderSize ? this.config.style.borderSize : null;
+                    let borderColor = this.config.style.borderColor ? this.config.style.borderColor : '#000';
+                    let borderRadius = this.config.style.borderRadius ? this.config.style.borderRadius : null;
+                    let padding = this.config.style.padding ? this.config.style.padding : null;
+                    let backgroundColor = this.config.style.backgroundColor ? this.config.style.backgroundColor : null;
+
+                    if (borderSize) { style.border = `${borderSize}px solid ${borderColor}` }
+                    if (borderRadius) { style.borderRadius = `${borderRadius}px` }
+                    if (padding) { style.padding = `${padding}px` }
+                    if (backgroundColor) { style.backgroundColor = `${backgroundColor}` }
+
+                    return style;
+                } else {
+                    return null
+                }
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .table {
+        /*transition: .3s ease;*/
+        /*&:hover {*/
+        /*    box-shadow: 0 0 5px rgba(1,1,1,0.2);*/
+        /*}*/
+    }
+    table {
+        border-spacing: 0;
+        width: 100%;
+
+    }
+    .row {
+        &:hover {
+            background-color: #f8f8f8;
+        }
+        &:first-child {
+            .el {
+                font-size: 18px;
+                font-weight: 700;
+                color: #4e4e4e;
+            }
+
+        }
+    }
+    .el {
+        border-bottom: 1px solid #d6d6d6;
+        border-right: 1px solid #d6d6d6;
+        padding: .5em 1em;
+        color: #252525;
+        font-size: 16px;
+        &:last-child {
+            border-right: none;
+        }
+    }
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="home">
-
+    <div style="width: 500px; margin-bottom: 1em;">
+      <v-table :config="configTable"></v-table>
+    </div>
   </div>
 </template>
 
 <script>
+  import vTable from '../components/library/table'
   export default {
     name: 'home',
     components: {
-
+      vTable
+    },
+    data() {
+      return {
+        configTable: {
+          style: {
+            borderSize: 1,
+            borderColor: '#d6d6d6',
+            borderRadius: 5,
+            padding: 20,
+            backgroundColor: '#fdfdfd'
+          },
+          content: [
+            {
+              row: [
+                {value: 'Title1'},
+                {value: 'Title2'},
+                {value: 'Title3'}
+              ]
+            },
+            {
+              row: [
+                {value: 'Value1'},
+                {value: 'Value2'},
+                {value: 'Value3'}
+              ]
+            }
+          ]
+        }
+      }
     }
   }
 </script>
